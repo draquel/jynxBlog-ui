d217cdcb93be918150f18180f7537d51ede59c4b
Dan Raquel <draquel@webjynx.com>
2021-06-19 19:06:05 -0400

    Added apiUrl to environment and implemented it in the post and tag service

--- src/app/shared/service/post.service.ts
+++ src/app/shared/service/post.service.ts
@@ -3,23 +3,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { map, catchError} from 'rxjs/operators';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
-  baseUrl = 'http://localhost:8080/v1/';
+  baseUrl = environment.apiUrl + 'posts';
 
   constructor(private http: HttpClient) { }
 
   getPost(id: number): Observable<any>{
-    return this.http.get(this.baseUrl + 'posts/' + id);
+    return this.http.get(this.baseUrl + '/' + id);
   }
 
   getPosts(params?: HttpParams): Observable<any> {
     if (params != null) {
-      return this.http.get(this.baseUrl + 'posts', {params});
+      return this.http.get(this.baseUrl, {params});
     }
-    return this.http.get(this.baseUrl + 'posts');
+    return this.http.get(this.baseUrl);
   }
 }
--- src/app/shared/service/tag.service.ts
+++ src/app/shared/service/tag.service.ts
@@ -3,24 +3,25 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { map, catchError} from 'rxjs/operators';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TagService {
 
-  baseUrl = 'http://localhost:8080/v1/';
+  baseUrl = environment.apiUrl + 'tags';
 
   constructor(private http: HttpClient) { }
 
   getTag(id: number): Observable<any>{
-    return this.http.get(this.baseUrl + 'tags/' + id);
+    return this.http.get(this.baseUrl + '/' + id);
   }
 
   getTags(params?: HttpParams): Observable<any> {
     if (params != null) {
-      return this.http.get(this.baseUrl + 'tags', {params});
+      return this.http.get(this.baseUrl, {params});
     }
-    return this.http.get(this.baseUrl + 'tags');
+    return this.http.get(this.baseUrl);
   }
 }
